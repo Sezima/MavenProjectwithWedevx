31f1674ffe9ead7a8db6e79dfe4b87a3e6f75271
sezim <osmsezimka@gmail.com>
2023-09-09 03:58:25 +0600

    just add comments for use other browsers

--- src/main/java/justForPractice/SeleniumIntro.java
+++ src/main/java/justForPractice/SeleniumIntro.java
@@ -42,3 +42,82 @@ public class SeleniumIntro {
 // And so java is  gonna send the command to the driver, and driver is gonna go deal with the browser driver is actually gonna send the comments to the browser
 // So there is this one extra file that is in between our selenium project and the browsers.
 
+
+
+
+//if i wanna do same thing with another browser i can just change implementation
+
+
+/*
+for  Internet Explorer (IE)
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class IETest {
+    public static void main(String[] args) {
+        // Set up InternetExplorerDriver using WebDriverManager
+        WebDriverManager.iedriver().setup();
+        WebDriver driver = new InternetExplorerDriver();
+
+        // Perform actions on IE
+        driver.get("https://www.example.com");
+        // ...
+
+        // Close the browser
+        driver.quit();
+    }
+}
+
+*/
+
+
+
+
+/*
+*for safari
+*
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class SafariTest {
+    public static void main(String[] args) {
+        // Set up SafariDriver
+        WebDriver driver = new SafariDriver();
+
+        // Perform actions on Safari
+        driver.get("https://www.example.com");
+        // ...
+
+        // Close the browser
+        driver.quit();
+    }
+}
+* */
+
+
+
+
+/*for firefox
+*
+* import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirefoxTest {
+    public static void main(String[] args) {
+        // Set up FirefoxDriver using WebDriverManager
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+
+        // Perform actions on Firefox
+        driver.get("https://www.example.com");
+        // ...
+
+        // Close the browser
+        driver.quit();
+    }
+}*/
+
+
