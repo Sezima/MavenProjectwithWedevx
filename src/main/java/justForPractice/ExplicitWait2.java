21aa7c50f557fe64371b6b29ec939eabfbb39be8
sezim <osmsezimka@gmail.com>
2023-09-17 03:32:44 +0600

    timeout explicit 5 sec Second Way

---
+++ src/main/java/justForPractice/ExplicitWait2.java
@@ -0,0 +1,34 @@
+package justForPractice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class ExplicitWait2 {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://demo-project.wedevx.co/");
+
+        WebElement dynamicProperties = driver.findElement(By.id("dynamicProperties_page"));
+        dynamicProperties.click();
+
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebElement secondway = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("visibleAfter")));
+//        WebElement visibleAfterButton = driver.findElement(By.id("visibleAfter"));
+//        visibleAfterButton.click();
+
+        secondway.click();
+
+
+    }
+}
