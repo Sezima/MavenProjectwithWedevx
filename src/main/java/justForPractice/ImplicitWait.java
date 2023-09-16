1c129c724759cfc652f24d9a43321200eb396ce8
sezim <osmsezimka@gmail.com>
2023-09-17 03:15:09 +0600

    timeout implicit

---
+++ src/main/java/justForPractice/ImplicitWait.java
@@ -0,0 +1,29 @@
+package justForPractice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ImplicitWait {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://demo-project.wedevx.co/");
+
+
+
+        WebElement currentAddress = driver.findElement(By.name("currentAddress"));
+        currentAddress.sendKeys("123 Main St");
+
+
+        WebElement perAddress = driver.findElement(By.name("permanentAddress"));
+        perAddress.sendKeys("456 Second St");
+
+
+    }
+}
