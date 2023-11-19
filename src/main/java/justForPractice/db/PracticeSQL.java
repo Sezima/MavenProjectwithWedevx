410e2e1cfa1ea222cf67d3099eb194fa4433d211
sezim <osmsezimka@gmail.com>
2023-11-20 05:48:17 +0600

    JDBC Statement And ResultSet Class Methods

--- src/main/java/justForPractice/db/PracticeSQL.java
+++ src/main/java/justForPractice/db/PracticeSQL.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class PracticeSQL {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         //database URL (ip or hostname), username, password
 
 //jdbs - is mandatory is not change
@@ -20,16 +20,20 @@ public class PracticeSQL {
 
 //tell the jdbs which driver to use
 
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
             //Establish
-            Connection connection = DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(url, username, password);
             // statement is a pre req to send a query
-            Statement statement = connection.createStatement();
+             statement = connection.createStatement();
 
             //execute select query  and save the result in an sql custom object
-            ResultSet resultSet = statement.executeQuery("select * from users");
+             resultSet = statement.executeQuery("select * from users");
 
             //result set returns a list of all rows
             //newxt method is like a pointer
@@ -45,10 +49,40 @@ public class PracticeSQL {
             resultSet.next();
             System.out.println(resultSet.getInt("profile_id"));
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+
+            System.out.println("===============dinamic code================");
+            while (resultSet.next()){
+                System.out.print(resultSet.getInt("id") + " |");
+                System.out.print(resultSet.getInt("account_non_expired") + " |");
+                System.out.print(resultSet.getInt("account_non_locked") + " |");
+                System.out.print(resultSet.getInt("credentials_non_expired") + " |");
+                System.out.print(resultSet.getInt("enabled") + " |");
+                System.out.print(resultSet.getString("username") + " |");
+                System.out.println(resultSet.getInt("profile_id") + " |");
+            }
+
+
+            int rowsAffected = statement.executeUpdate("UPDATE user_profile SET address = 'Canggu Bali' WHERE email_address = 'elon@tesla.com'");
+            System.out.println(rowsAffected);
+            System.out.println(rowsAffected);
+
+            resultSet = statement.executeQuery("select * from user_profile");
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("address"));
+                System.out.println(resultSet.getString("email_address"));
+
+            }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+
+        } finally {
+            try {
+                connection.close();
+                statement.close();
+                resultSet.close();
+            } catch (SQLException throwables){
+                throwables.printStackTrace();
+            }
 
         }
     }
